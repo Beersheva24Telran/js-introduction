22113cfcc78c50ffc651b6a234ba340f5cb6c49f
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-22 16:07:19 +0200

    challenge simplified myBind

--- functions.mjs
+++ functions.mjs
@@ -1,31 +1,4 @@
-// function sum(num1, num2) {
-//     return this.x * num1 + this.x * num2;
-// }
-// sum.a = 2;
-// sum.call({x:5}, 10, 10);
-const point = {
-    x: 5,
-    y:10
-
-
-};
-function displayPoint(z,d,f,g) {
-    console.log(`x:${this.x}; y:${this.y}; z:${arguments[0]}`)
-
+export function myBind(thisArg) {
+    //TODO
+    //apply method "apply"
 }
-const display = displayPoint.bind(point);
-display(40,"abc",30, true);
-
-const displayPointArrow = (z) => {
-    console.log(`x:${this.x}; y:${this.y}; z:${z}`)
-}
-//Arrow function doesn't have its own this
-// const displayArrow = displayPointArrow.bind(point);
-// displayArrow(40);
-display.call(point, [40, 30, true, false]);
-// display.apply(point, [40, 30, true, false])
-
-
-
-
-
---
+++ test/functions.test.mjs
@@ -0,0 +1,17 @@
+import {describe, it, expect} from 'vitest';
+import { myBind } from '../functions.mjs';
+describe("myBind test according to the TODO comments", () => {
+    const point = {x:3, y:4};
+    function sumArguments(num3=0, num4=0) {
+        return this.x + this.y + num3 + num4;
+    }
+    sumArguments.bind = myBind;
+    const fun = sumArguments.bind(point);
+    it("no additional parameters passed", () => {
+        
+        expect(fun()).toBe(7);
+    })
+    it(" all parameters are passed ", () => {
+        expect(fun(10, 20)).toBe(37)
+    })
+})
