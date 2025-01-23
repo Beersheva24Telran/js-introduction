f3ea652dea8c3dea71482d138fc9de11076e1f20
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-23 14:48:24 +0200

    spread operator for object

--- test/objects.test.mjs
+++ test/objects.test.mjs
@@ -1,41 +1,23 @@
 import {describe, expect, it} from 'vitest';
-describe("object iterating", () => {
-    const person = {name: "Vasya", age: 25}
-    it("for-in iterating", () => {
-        const keys = ["name", "age"];
-        const actual = [];
-        let index = 0;
-        for(let key in person) {
-            actual[index++] = key;
-        }
-        expect(actual).toEqual(keys)
-    })
-    it("keys iterating using for...of", () => {
-        const keys = ["name", "age"];
-        const actual = [];
-        let index = 0;
-        for(let key of Object.keys(person)) {
-            actual[index++] = key;
-        }
-        expect(actual).toEqual(keys)
-    });
-    it("values iterating using for...of construction", () => {
-        const expectedValues = ["Vasya", 25];
-        const actualValues = [];
-        let index = 0;
-        for(let value of Object.values(person)) {
-            actualValues[index++] = value;
-        }
-        expect(actualValues).toEqual(expectedValues)
-    });
-    it("iterating entries using for...of construction", () => {
-       const expectedEntries = [["name", "Vasya"], ["age", 25]]
-       const actualEntries = [];
-       let index = 0;
-       for(let entry of Object.entries(person)) {
-            actualEntries[index++] = entry;                                                                                                                             
-       }
-       expect(actualEntries).toEqual(expectedEntries)
-    })
+describe ("Object copying", () => {
+    
+   it("showing example of references assignment but not copy", () => {
+    const person1 = {name: "Vasya", age:25};
+     const person2 = person1;
+    person2.gender="male";
+    expect(person1.gender).toBe("male")
+
+   })
+   it("copying using spread operator", () => {
+    const person1 = {name: "Vasya", age:25};
+    const person2 = {...person1}
+    person2.city="Lod";
+    expect(person1.city).toBeUndefined();
+    person1.city="Lod";
+    expect(person1 != person2).toBeTruthy();
+    expect(person2).toEqual(person1);
+    expect(person2).not.toBe(person1)
+   });
+  
 })
 
