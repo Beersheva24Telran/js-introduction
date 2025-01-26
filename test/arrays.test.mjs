d0ceb41f8b776b8e79e83d02ae7fd70bc03e61df
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-26 14:26:48 +0200

    sorting

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,24 +1,9 @@
 import {describe, it, expect} from 'vitest'
-describe("iterating elements of array", () => {
-    const ar = [1, 2, 3, 4];
-    it("printing out elements using for-in", () => {
-       for(let i in ar) {
-        console.log(ar[i]);
-       }
-
-        
-    })
-    it("printing out elements using for-of", () => {
-        for(let num of ar) {
-         console.log(num);
-        }
-     })
-     it ("printing out elements using forEch method", () => {
-        ar.forEach(e => console.log(e));
-     })
-     it ("printing out index and element from array", () => {
-        ar.forEach((e, i) => console.log(`index: ${i}; element: ${e}`))
-     })
-    
-    
+describe("sorting array", () => {
+   const array = [10, 1000, -10, 30, 60]
+   it("numbers native sorting soting", () => {
+        const expected = [-10, 10,30, 60, 1000 ];
+        expect(array.toSorted((a, b) => a - b)).toEqual(expected);
+   })
+   
 })
