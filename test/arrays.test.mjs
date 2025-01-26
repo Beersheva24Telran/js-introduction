ff46c93d0510eaaea41bbbd65d9d708321f4e990
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-26 12:03:52 +0200

    removing elements from array

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,22 +1,22 @@
 import {describe, it, expect} from 'vitest'
-describe("inserting new elements in array", () => {
-    const inseredNumbers = [-10, -20];
-    it("inserting at begining of array", () => {
-        const ar = [1, 2];
-        const expected = [-10, -20, 1, 2];
-        ar.unshift(...inseredNumbers);
+describe("removing elements from array array", () => {
+    
+    it("removing first element", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [ 2, 3, 4];
+        ar.shift();
         expect(ar).toEqual(expected);
     })
-    it("adding new numbers at end of array", () => {
-        const ar = [1, 2];
-        const expected = [ 1, 2,-10, -20];
-        ar.push(...inseredNumbers);
+    it("removing last element", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [ 1, 2, 3];
+        ar.pop();
         expect(ar).toEqual(expected);
     })
-    it("inserting new number at middle of array", () => {
-        const ar = [1, 2];
-        const expected = [ 1, -10, -20, 2];
-        ar.splice(1,0,...inseredNumbers)
+    it("removing elements from middle of array", () => {
+        const ar = [1, 2, 3, 4];
+        const expected = [ 1, 4];
+        ar.splice(1,2)
         expect(ar).toEqual(expected);
     })
 })
