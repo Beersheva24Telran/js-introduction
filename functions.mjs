17e5aa4a6e65315fae5266e0c9b4cd38c07f647d
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-26 11:55:16 +0200

    mybind solution

--- functions.mjs
+++ functions.mjs
@@ -1,5 +1,11 @@
-export function myBind(thisArg, ...otherArgs) {
-   //TODO
+export function myBind(thisArg, ...bindArgs) {
    //write method myBind fully running standard "bind" method functionality
    //no using standard call / apply methods
+   return (...args) => {
+      const fieldTime = new Date().getTime();
+      thisArg[fieldTime] = this;
+      const res = thisArg[fieldTime](...args, ...bindArgs);
+      delete thisArg[fieldTime];
+      return res;
+   }
 }
