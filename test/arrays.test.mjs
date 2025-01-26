e500537ed9c6ce6c3c3af7e90d2b489e7e1465ee
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-26 15:43:37 +0200

    slice/ join/ split

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,25 +1,17 @@
 import {describe, it, expect} from 'vitest'
 const array = [10, 1000, -10, 30, 60];
-describe("array introspecting", () => {
-  
-   const objects = [
-    {x:4},
-    {x:5}
-]
-   it(" includes", () => {
-        
-        expect(array.includes(1000)).toBeTruthy();
-        expect(array.includes(200)).toBeFalsy();
-        expect(array.includes({x:4})).toBeFalsy();
-   });
-   it(" testing for array of objects", () => {
-        expect(objects.find(obj => obj.x === 4)).toBeTruthy();
-        expect(objects.find(obj => obj.x === 1)).toBeFalsy();
-   })
-   it ("testing for all elements match acondition", () => {
-    expect(array.every(num => num % 2 === 0)).toBeTruthy();
-    expect(array.some(num => num % 2 !== 0)).toBeFalsy();
-   
-   } )
-
+describe("slice, join, string-split", () => {
+    it("slice method", () => {
+        const array = [1, 2, 3, 4, 5];
+        const expected  = [2, 3, 4];
+        expect(array.slice(1, 4)).toEqual(expected);
+        const copyExpected = [1, 2, 3, 4, 5];
+        expect(array.slice()).toEqual(copyExpected);
+    })
+    it("join method", () => { 
+        const expectedStr = "10;1000;-10;30;60";
+        expect(array.join(";")).toBe(expectedStr);
+        const arStr = expectedStr.split(/;/);
+        expect(arStr).toEqual(array.map(s => s.toString()));
+    })
 })
