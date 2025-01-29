dc7eb76c2114f4ef00effb2709be6e4af95bb991
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-29 11:19:51 +0200

    array destructuring

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,16 +1,17 @@
 import {describe, it, expect} from 'vitest'
-import { myMap, myReduce } from '../arrays.mjs';
 const array = [10, 1000, -10, 30, 60];
-describe("map / reduce", () => {
-    array.map = myMap;
-    array.reduce = myReduce;
-    it("myMap testing", () => {
-       const expected = [10, 1001, -8, 33, 64]//array of elements where each element will be converted to element + index
-       expect(array.map((e,index) => e + index)).toEqual(expected);
-    })
-    it("myReduce testing", () => {
-        //TODO
-        //write two reduce methods for taking sum of all array numbers
-        //and minimal and maximal values for only one reduce call
-          })
+describe("destructuring array", () => {
+  it("assigning two first numbers of array to different variables", () => {
+    const [a, b] = array;
+    expect(a).toBe(array[0]);
+    expect(b).toBe(array[1]);
+  })
+  it("swap of values by using destructuring", () => {
+    let a = 10;
+    let b = 20;
+    // const ar = [a, b];
+    [b, a] = [a, b];
+    expect(a).toBe(20);
+    expect(b).toBe(10);
+  })
 })
