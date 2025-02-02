4160cab2aaf13c2f682447bdcc5e1e4944feda9e
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-02 11:03:55 +0200

    interview deferred task

---
+++ index.js
@@ -0,0 +1,19 @@
+class Deferred {
+    #functions
+    constructor() {
+        this.#functions = [];
+    }
+    then(fun) {
+        this.#functions.push(fun);
+    }
+    resolve(value) {
+        this.#functions.forEach(fun => value = fun(value));
+    }
+}
+const d = new Deferred()
+d.then(function(res){ console.log("1 ", res); return "a"; });
+
+d.then(function(res){ console.log("2 ", res); return "b"; });
+
+d.then(function(res){ console.log("3 ", res); return "c"; });
+d.resolve('hello');
