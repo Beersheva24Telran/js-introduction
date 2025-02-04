d2f753eaeb05e309a95a6e5ddf8e90a81564fbe4
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-04 16:03:20 +0200

    iterating based on iterator and generator function

--- objects.mjs
+++ objects.mjs
@@ -7,5 +7,17 @@ export function makeIteratorClosedRange(min, max) {
     }
 }
 export function makeIterableClosedRange(min, max) {
-    //TODO
+    return {
+        [Symbol.iterator]:makeIteratorClosedRange.bind(undefined, min,max)
+    }
+}
+export function* makeGeneratorClosedRange(min, max) {
+    for(let i = min; i <= max; i++) {
+        yield i;
+    }
+}
+export function makeIterableBasedGenerator(min, max) {
+    return {
+        [Symbol.iterator]:makeGeneratorClosedRange.bind(undefined,min, max)
+    }
 }
--- test/objects.test.mjs
+++ test/objects.test.mjs
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
-import { makeIteratorClosedRange, makeIterableClosedRange } from "../objects.mjs";
+import { makeIteratorClosedRange, makeIterableClosedRange, makeGeneratorClosedRange, makeIterableBasedGenerator } from "../objects.mjs";
 describe ("iterator pattern test", () => {
-  it("iterator test", ()=>{
+  it("iterator test based on making iterator", ()=>{
     //
       const iterator = makeIteratorClosedRange(1, 3);
       const expected = [1, 2, 3];
@@ -16,16 +16,45 @@ describe ("iterator pattern test", () => {
       }
       expect(actual).toEqual(expected);
   });
-  it("terable test", () => {
+  it("terable test based on iterator", () => {
     const iterable = makeIterableClosedRange(1, 3);
     const expected = [1, 2, 3];
     const actual = [];
-    for(const num of iterable) {
+    for (const num of iterable) {
       actual.push(num);
     }
     expect(actual).toEqual(expected);
     actual.length = 0;
-    for(const num of iterable) {
+    for  (const num of iterable) {
+      actual.push(num);
+    }
+    expect(actual).toEqual(expected);
+  })
+  it("iterator test based on generator function", ()=>{
+    //
+      const iterator = makeGeneratorClosedRange(1, 3);
+      const expected = [1, 2, 3];
+      const actual = [];
+      
+      while(true) {
+          const {value, done} = iterator.next();
+          if(done) {
+            break;
+          }
+          actual.push(value)
+      }
+      expect(actual).toEqual(expected);
+  });
+  it("terable test based on generator function", () => {
+    const iterable = makeIterableBasedGenerator(1, 3);
+    const expected = [1, 2, 3];
+    const actual = [];
+    for (const num of iterable) {
+      actual.push(num);
+    }
+    expect(actual).toEqual(expected);
+    actual.length = 0;
+    for  (const num of iterable) {
       actual.push(num);
     }
     expect(actual).toEqual(expected);
